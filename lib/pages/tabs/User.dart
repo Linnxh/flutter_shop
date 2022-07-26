cbde77d115687b38e2f8b7c3cb81b757d0d23f17
linxiuhong <wbxiuhong.lin@transsion.com>
2022-07-26 16:34:46 +0800

    feat--customView

--- lib/pages/tabs/User.dart
+++ lib/pages/tabs/User.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class UserPage extends StatefulWidget {
   const UserPage({Key? key}) : super(key: key);
@@ -10,6 +11,62 @@ class UserPage extends StatefulWidget {
 class _UserPageState extends State<UserPage> {
   @override
   Widget build(BuildContext context) {
-    return Text("user");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("User"),
+      ),
+      body: ListView(
+        children: [
+          InkWell(
+            child: Container(
+              height: 50,
+              color: Colors.redAccent,
+              alignment: Alignment.center,
+              child: Text(
+                "CustomView Rotated",
+                style: TextStyle(fontSize: 15,color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            onTap: () {
+              Navigator.pushNamed(context, '/customViewRotated',
+                  arguments: {"id": "testhaha"});
+            },
+          ),
+          InkWell(
+            child: Container(
+              height: 50,
+              color: Colors.yellowAccent,
+              alignment: Alignment.center,
+              child: Text(
+                "CustomView Paint Canvas",
+                style: TextStyle(fontSize: 15,color: Colors.black),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            onTap: () {
+              Navigator.pushNamed(context, '/customPaint',
+                  arguments: {"id": "testhaha"});
+            },
+          ),
+          InkWell(
+            child: Container(
+              height: 50,
+              color: Colors.greenAccent,
+              alignment: Alignment.center,
+              child: Text(
+                "CustomView checkbox",
+                style: TextStyle(fontSize: 15,color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            onTap: () {
+              Navigator.pushNamed(context, '/customCheckBox',
+                  arguments: {"id": "testhaha"});
+            },
+          )
+        ],
+      ),
+    );
   }
 }
